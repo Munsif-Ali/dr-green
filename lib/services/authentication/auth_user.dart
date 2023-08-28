327ee5a7161777eefafe9aaa84c4ade52e1869cc
Munsif Ali <munsifalitaj@gmail.com>
2023-08-28 18:42:27 +0500

    add name and photourl to authUser

--- lib/services/authentication/auth_user.dart
+++ lib/services/authentication/auth_user.dart
@@ -3,15 +3,21 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthUser {
   final String id;
   final String email;
+  final String name;
+  final String? photoUrl;
   // final bool isEmailVerified;
   const AuthUser({
     required this.id,
     required this.email,
+    required this.name,
+    this.photoUrl,
     // required this.isEmailVerified,
   });
   factory AuthUser.fromFirebase(User user) => AuthUser(
         id: user.uid,
         email: user.email!,
+        name: user.displayName!,
+        photoUrl: user.photoURL,
         // isEmailVerified: user.emailVerified,
       );
 }
