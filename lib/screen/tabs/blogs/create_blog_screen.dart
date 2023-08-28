abd7c91a58e1a87323d2c6af1f7342575471046b
Munsif Ali <munsifalitaj@gmail.com>
2023-08-28 18:29:11 +0500

    create blog funcionality done

--- lib/screen/tabs/blogs/create_blog_screen.dart
+++ lib/screen/tabs/blogs/create_blog_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:doctor_green/constants/globals_variables.dart';
 import 'package:doctor_green/models/blogs_model.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as fStorage;
 
 class CreateBlogScren extends StatefulWidget {
   CreateBlogScren({super.key});
@@ -28,62 +30,67 @@ class _CreateBlogScrenState extends State<CreateBlogScren> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Form(
-            child: Column(
-              children: [
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: _titleController,
-                  decoration: const InputDecoration(
-                    hintText: "Title",
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(
+                      hintText: "Title",
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                //write code to pick image for blog
-                _imageFile == null
-                    ? const Text("No image selected")
-                    : Image.file(
-                        File(_imageFile!.path),
-                        height: 200,
-                        width: 200,
-                      ),
-                OutlinedButton.icon(
-                  onPressed: () async {
-                    final ImagePicker picker = ImagePicker();
-                    final XFile? image = await picker.pickImage(
-                      source: ImageSource.gallery,
-                    );
-                    setState(() {
-                      _imageFile = image;
-                    });
-                  },
-                  label: const Text("Pick Image"),
-                  icon: const Icon(Icons.image),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: _bodyController,
-                  maxLines: 5,
-                  keyboardType: TextInputType.multiline,
-                  decoration: const InputDecoration(
-                    // label: Text("Body"),
-                    hintText: "Blog Description",
+                  const SizedBox(height: 20),
+                  //write code to pick image for blog
+                  _imageFile == null
+                      ? const Text("No image selected")
+                      : Image.file(
+                          File(_imageFile!.path),
+                          height: 200,
+                          width: 200,
+                        ),
+                  OutlinedButton.icon(
+                    onPressed: () async {
+                      final ImagePicker picker = ImagePicker();
+                      final XFile? image = await picker.pickImage(
+                        source: ImageSource.gallery,
+                      );
+                      setState(() {
+                        _imageFile = image;
+                      });
+                    },
+                    label: const Text("Pick Image"),
+                    icon: const Icon(Icons.image),
                   ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () async {
-                    final String title = _titleController.text;
-                    final String body = _bodyController.text;
-                    final BlogsModel blog = BlogsModel(
-                      title: title,
-                      body: body,
-                    );
-                    createPost(blog);
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text("Submit"),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _bodyController,
+                    maxLines: 5,
+                    keyboardType: TextInputType.multiline,
+                    decoration: const InputDecoration(
+                      // label: Text("Body"),
+                      hintText: "Blog Description",
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final String title = _titleController.text;
+                      final String body = _bodyController.text;
+                      final user = firebaseAuth.currentUser;
+                      final BlogsModel blog = BlogsModel(
+                        title: title,
+                        body: body,
+                        imageUrl: _imageFile!.path,
+                        userId: user?.uid,
+                      );
+                      createPost(blog);
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Submit"),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -91,5 +98,21 @@ class _CreateBlogScrenState extends State<CreateBlogScren> {
     );
   }
 
-  void createPost(BlogsModel blog) async {}
+  void createPost(BlogsModel blog) async {
+    // store blog image in firebase storage then get the url of the image and store the blog in firestore
+
+    String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+    fStorage.Reference reference =
+        fStorage.FirebaseStorage.instance.ref().child("blogs").child(fileName);
+    fStorage.UploadTask uploadTask = reference.putFile(File(_imageFile!.path));
+    fStorage.TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() {});
+    await taskSnapshot.ref.getDownloadURL().then((url) {
+      blog.imageUrl = url;
+    });
+    // store blog in firestore
+
+    final doc = firebaseFirestore.collection("blogs").doc();
+    blog.id = doc.id;
+    await doc.set(blog.toJson());
+  }
 }
