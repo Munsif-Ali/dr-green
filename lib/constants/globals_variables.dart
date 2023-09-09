f1c4d3455ef3ae50561c76e2a5010d9be88981ba
Munsif Ali <munsifalitaj@gmail.com>
2023-09-09 15:43:09 +0500

    navigator key added to material app

--- lib/constants/globals_variables.dart
+++ lib/constants/globals_variables.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 export 'package:shared_preferences/shared_preferences.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
 
 SharedPreferences? sharedPreferences;
 final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 final FirebaseStorage firebaseStorage = FirebaseStorage.instance;
 final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+final navigatorKey = GlobalKey<NavigatorState>();
